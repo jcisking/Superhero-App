e47b7cca839ceee77b9d1d14ae90cbf4f41787ab
jcisking <krisloeffler@gmail.com>
2023-01-05 21:54:46 -0700

    another commit

--- app/src/main/java/com/example/superheroapp/HeroesDataScource.kt
+++ app/src/main/java/com/example/superheroapp/HeroesDataScource.kt
@@ -1,36 +1,45 @@
 package com.example.superheroapp
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import com.example.superheroapp.data.Hero
 
 @Composable
-fun HeroItem() {
-    Card() {
-        Row() {
-            HeroInformation()
-            HeroIcon()
-            
+fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.padding(20.dp)) {
+        Row(
+            modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+        ) {
+            HeroInformation(modifier = Modifier.weight(1f), heroName = hero.nameRes, heroDescription = hero.descriptionRes)
+            HeroIcon(heroImage = hero.imageRes)
         }
-        
     }
-
 }
 
 @Composable
-fun HeroIcon() {
-    Image(painter =, contentDescription =)
+fun HeroInformation(@StringRes heroName: Int, heroDescription: Int, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = stringResource(id = heroName))
+        Text(text = stringResource(id = heroDescription))
+    }
 }
 
 @Composable
-fun HeroInformation() {
-    Column {
-        Text(text =)
-        Text(text =)
-
-    }
+fun HeroIcon(@DrawableRes heroImage: Int, modifier: Modifier = Modifier) {
+    Image(modifier = modifier
+        .size(40.dp)
+        .padding(0.dp), painter = painterResource(id = heroImage), contentDescription = null)
 }
--- app/src/main/java/com/example/superheroapp/MainActivity.kt
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheroapp.data.Hero
+import com.example.superheroapp.data.HeroesRepository
 import com.example.superheroapp.ui.theme.SuperHeroAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HeroApp()
+
                 }
             }
         }
@@ -30,14 +35,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HeroApp() {
+    LazyColumn() {
+        this.items(HeroesRepository.heroes) {
+            HeroItem(hero = it)
+
+        }
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SuperHeroAppTheme {
-        Greeting("Android")
+
     }
 }
